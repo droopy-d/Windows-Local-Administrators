a37b491f4d29eab8388426bbcde671d18e3f6603
mircea-g <80977959+mircea-g@users.noreply.github.com>
2021-10-22 15:30:30 +0200

    Update Windows-Local-Administrators.ps1

--- Windows-Local-Administrators.ps1
+++ Windows-Local-Administrators.ps1
@@ -8,32 +8,30 @@ if (!(Get-Module ActiveDirectory)) {Import-Module ActiveDirectory}
 
 # Get list of servers
 $Servers = Get-ADComputer -properties OperatingSystem -Filter 'OperatingSystem -like "windows*server*"' | Where-Object {$_.DistinguishedName -notlike "*Domain Controllers*"} | sort DNSHostName | select DNSHostName -ExpandProperty DNSHostName
-
+Write-Host
+Write-Host "Ping each computer:"
 $ServersUp = @()
 ForEach ($server in $Servers){
     if ((Test-Connection $server -Quiet) -eq $True){
-        Write-Host -ForegroundColor Green "$server up"
+        Write-Host -ForegroundColor Green "$server ping ok."
         $ServersUp += $server
     }
     else{
-        Write-Host -ForegroundColor red "$server down. Will be skipped."
+        Write-Host -ForegroundColor red "$server ping fails. Will be skipped."
     }
 }
 
-try{
-    invoke-command {
-        $members = net localgroup administrators |
-        where {$_ -AND $_ -notmatch "command completed successfully"} |
-        select -skip 4
-        New-Object PSObject -Property @{
-            Computername = [System.Net.DNS]::GetHostByName('').HostName
-            Group = "Administrators"
-            Members=$members
-        }
-    } -computer $ServersUp -HideComputerName |
-    Select Computername,@{N='Members';E={($_.Members) -join ';'}} -ExcludeProperty RunspaceID,PSShowComputerName,PSComputerName | 
-    Export-CSV -NoTypeInformation -Append $location\Local_Administrators_$timeStamp.csv
-    Write-Host "Collect the report at $location\Local_Administrators_$timeStamp.csv"
-}
-catch{
-}
+invoke-command {
+    $members = net localgroup administrators |
+    where {$_ -AND $_ -notmatch "command completed successfully"} |
+    select -skip 4
+    New-Object PSObject -Property @{
+        Computername = [System.Net.DNS]::GetHostByName('').HostName
+        Group = "Administrators"
+        Members=$members
+    }
+} -computer $ServersUp -HideComputerName |
+Select Computername,@{N='Members';E={($_.Members) -join ';'}} -ExcludeProperty RunspaceID,PSShowComputerName,PSComputerName | 
+Export-CSV -NoTypeInformation -Append $location\Local_Administrators_$timeStamp.csv
+Write-Host
+Write-Host "Collect the report at $location\Local_Administrators_$timeStamp.csv"
